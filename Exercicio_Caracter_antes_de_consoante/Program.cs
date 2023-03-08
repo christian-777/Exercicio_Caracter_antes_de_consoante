34d61e6f0213191e356f0cc3e1c474764d1b2d94
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-08 00:41:16 -0300

    Add project files.

---
+++ Exercicio_Caracter_antes_de_consoante/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool valida=false;
+        string parametro, resultado="", letra;
+        string consoante = "bcdfghjklmnpqrstvwxyz";
+
+        Console.WriteLine("digite o parametro a ser auterado: ");
+        parametro = Console.ReadLine();
+
+        Console.WriteLine("digite a letra que deseja inserir: ");
+        letra = Console.ReadLine();
+
+        for(int i = 0; i <parametro.Length; i++)
+        {
+            valida = false;
+            for(int j = 0; j <consoante.Length; j++) 
+            {
+                if (parametro[i] == consoante[j])
+                {
+                    valida = true;
+                    break;
+                }
+            }
+            if (valida)
+            {
+                resultado += letra;
+                resultado += parametro[i];
+            }
+            else
+            {
+                resultado += parametro[i];
+            }
+        }
+        Console.WriteLine(resultado);
+
+    }
+}
